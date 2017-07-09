3d6da08a33eb87a7cbd04acc1b08bc5d7f992f92
yionlee <performizer@gmail.com>
2017-07-10 00:49:54 +0900

    redis with express

--- modules/urlModule.js
+++
@@ -1,7 +0,0 @@
-var url = require("url");
-var parseURL = url.parse("http://www.example.com/profile?name=history");
-
-
-console.log("protocol : " + parseURL.protocol);
-console.log("host : " + parseURL.host);
-console.log("query : " + parseURL.query);
--- readme.md
+++
@@ -1,7 +0,0 @@
-
-npm install express --save 
-npm install ejs --save 
-
-server start : node app.js
-
-nodemon app.js 
